379c598ce003127a6cfcb2bb5194409e9d050d82
MadhuriMalgaya <129099016+MadhuriMalgaya@users.noreply.github.com>
2024-07-12 16:00:15 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -6,11 +6,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
     mapping(address => string[]) private _purchases;
+    mapping(string => uint256) private _itemPrices;
 
     event Burn(address indexed from, uint256 value);
     event Redeem(address indexed from, uint256 value, string itemName);
 
-    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {}
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
+
+        _itemPrices["tea"] = 10;
+        _itemPrices["coffee"] = 15;
+        _itemPrices["chips"] = 5;
+        _itemPrices["samosha"] = 7;
+        _itemPrices["cookies"] = 8;
+    }
 
     function mint(address to, uint256 value) public onlyOwner {
         _mint(to, value);
@@ -20,13 +28,15 @@ contract DegenToken is ERC20, Ownable {
         return super.transfer(to, value);
     }
 
-    function redeem(string memory itemName, uint256 value) public {
+    function redeem(string memory itemName) public {
         address sender = msg.sender;
-        require(balanceOf(sender) >= value, "Insufficient balance");
+        uint256 itemPrice = _itemPrices[itemName];
+        require(itemPrice > 0, "Item does not exist");
+        require(balanceOf(sender) >= itemPrice, "Insufficient balance");
 
-        _burn(sender, value);
+        _burn(sender, itemPrice);
         _purchases[sender].push(itemName);
-        emit Redeem(sender, value, itemName);
+        emit Redeem(sender, itemPrice, itemName);
     }
 
     function burn(uint256 value) public {
@@ -36,5 +46,13 @@ contract DegenToken is ERC20, Ownable {
 
     function getPurchases(address account) public view returns (string[] memory) {
         return _purchases[account];
+    }
+
+    function getItemPrice(string memory itemName) public view returns (uint256) {
+        return _itemPrices[itemName];
+    }
+
+    function setItemPrice(string memory itemName, uint256 price) public onlyOwner {
+        _itemPrices[itemName] = price;
     }
 }
